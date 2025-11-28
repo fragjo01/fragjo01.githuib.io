8528897ffa87899e93c12ca36770b8ec0be5f4fa
fragjo01 <fragjo01@luther.edu>
2025-11-28 14:37:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,16 +15,8 @@ const screenshots = [
   "assets/tennisproject/Screenshot 2025-11-28 at 13.59.51.png",
   "assets/tennisproject/Screenshot 2025-11-28 at 13.59.59.png",
   "assets/tennisproject/Screenshot 2025-11-28 at 14.00.07.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.00.24.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.00.32.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.00.40.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.00.49.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.00.56.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.01.04.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.01.12.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.01.21.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.01.40.png",
-  "assets/tennisproject/Screenshot 2025-11-28 at 14.02.17.png"
+  "assets/tennisproject/Screenshot 2025-11-28 at 14.00.24.png"
+  // Add more if needed
 ];
 
 // Lightbox
